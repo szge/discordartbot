15d95411278b9b0f2ba8d76c13dd8fe61010d7bf
Jonathan <jchiangconan@gmail.com>
2021-07-01 01:14:16 -0400

    function returned early like a dumbass but i fixed it

--- index.js
+++ index.js
@@ -38,68 +38,68 @@ function dailyCheck() {
 
 client.on('message', msg => {
     if (msg.author.bot) return;
-    if (!msg.content.startsWith('artbot')) return;
-    // commands
-    switch (msg.content) {
-        case 'artbot': {
-            msg.reply('artbot v1.2 for commands type \'artbot help\'');
-            break;
-        }
-        case 'artbot help': {
-            var help_text = '__**Available commands:**__\n';
-            help_text += '**artbot ping**: test if the bot is working\n';
-            help_text += '**artbot embed test**\n';
-            help_text += '**artbot get channel**: get information about the channel\n';
-            help_text += '**artbot admin test**\n';
-            help_text += '**artbot kick test**\n';
-            help_text += '**artbot clear assignment submissions**: [admin] clear the assignment submission role manually '
-            help_text += '(ONLY if you know what you\'re doing)\n';
-            msg.reply(help_text);
-            break;
-        }
-        case 'artbot ping': {
-            msg.reply('pong');
-            break;
-        }
-        case 'artbot embed test': {
-            const embed = new Discord.MessageEmbed()
-            .setTitle('A slick little embed')
-            .setColor(0xff0000)
-            .setDescription('Hello, this is a slick embed!');
-            msg.channel.send(embed);
-            break;
-        }
-        case 'artbot get channel': {
-            msg.reply('this channel is ' + msg.channel.name + ' id: ' + msg.channel.id);
-            break;
-        }
-        case 'artbot admin test': {
-            if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-                msg.reply('you are an admin');
-            } else {
-                msg.reply('you do not have permissions to do that (admin)');
+    if (msg.content.startsWith('artbot')){// commands
+        switch (msg.content) {
+            case 'artbot': {
+                msg.reply('artbot v1.2 for commands type \'artbot help\'');
+                break;
             }
-            break;
-        }
-        case 'artbot kick test': {
-            if (msg.member.hasPermission(Discord.Permissions.FLAGS.KICK_MEMBERS)) {
-                msg.reply('you can kick people');
-            } else {
-                msg.reply('you do not have permissions to do that');
+            case 'artbot help': {
+                var help_text = '__**Available commands:**__\n';
+                help_text += '**artbot ping**: test if the bot is working\n';
+                help_text += '**artbot embed test**\n';
+                help_text += '**artbot get channel**: get information about the channel\n';
+                help_text += '**artbot admin test**\n';
+                help_text += '**artbot kick test**\n';
+                help_text += '**artbot clear assignment submissions**: [admin] clear the assignment submission role manually '
+                help_text += '(ONLY if you know what you\'re doing)\n';
+                msg.reply(help_text);
+                break;
             }
-            break;
-        }
-        case 'artbot clear assignment submissions': {
-            if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-                msg.reply('test');
-            } else {
-                msg.reply('you do not have permissions to do that (admin)');
+            case 'artbot ping': {
+                msg.reply('pong');
+                break;
             }
-            break;
-        }
-        default: {
-            msg.reply('sorry, your command was not recognized');
-            break;
+            case 'artbot embed test': {
+                const embed = new Discord.MessageEmbed()
+                .setTitle('A slick little embed')
+                .setColor(0xff0000)
+                .setDescription('Hello, this is a slick embed!');
+                msg.channel.send(embed);
+                break;
+            }
+            case 'artbot get channel': {
+                msg.reply('this channel is ' + msg.channel.name + ' id: ' + msg.channel.id);
+                break;
+            }
+            case 'artbot admin test': {
+                if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
+                    msg.reply('you are an admin');
+                } else {
+                    msg.reply('you do not have permissions to do that (admin)');
+                }
+                break;
+            }
+            case 'artbot kick test': {
+                if (msg.member.hasPermission(Discord.Permissions.FLAGS.KICK_MEMBERS)) {
+                    msg.reply('you can kick people');
+                } else {
+                    msg.reply('you do not have permissions to do that');
+                }
+                break;
+            }
+            case 'artbot clear assignment submissions': {
+                if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
+                    msg.reply('test');
+                } else {
+                    msg.reply('you do not have permissions to do that (admin)');
+                }
+                break;
+            }
+            default: {
+                msg.reply('sorry, your command was not recognized');
+                break;
+            }
         }
     }
 
@@ -112,8 +112,8 @@ client.on('message', msg => {
         msg.member.roles.add(assignmentSubRole);
     } else if (msg.channel.id == figuresChannel && msg.attachments.size > 0) {
         // submitted figures
-        var message = 'your assignment submission has been acknowledged! good job\n';
-        message += '(if you don\'t have the "posted in assignments" role message a mod to report a bot issue)';
+        var message = 'your figures submission has been acknowledged! good job\n';
+        message += '(if you don\'t have the "posted in figures" role message a mod to report a bot issue)';
         msg.reply(message);
         msg.member.roles.add(figuresSubRole);
     }
