b183a08b723da956dbea79e4f368b3f6cc67f882
Jonathan <jchiangconan@gmail.com>
2021-07-01 01:08:26 -0400

    added figures stuff, code cleanup

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const client = new Discord.Client();
 
 var didDailyCheck = false;
 
-// list of submission channels
-const submissionList = [
+// list of assignment submission channels
+const assignmentList = [
     "859266797680984095", // test channel
     "853820913955110942", // colour theory
     "853822141992796191", // environments
@@ -20,9 +20,12 @@ const submissionList = [
     "853829700051664897"  // storyboard
 ];
 
-const adminRole = "853808936133591080";
+// figures submission channel
+const figuresChannel = "853855324259745792";
 
-const submittedRole = "859794283878678568";
+const assignmentSubRole = "859794283878678568";
+
+const figuresSubRole = "858450559852740609";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -35,24 +38,30 @@ function dailyCheck() {
 
 client.on('message', msg => {
     if (msg.author.bot) return;
-
+    if (!msg.content.startsWith('artbot')) return;
     // commands
     switch (msg.content) {
-        case 'ping': {
-            msg.reply('pong');
+        case 'artbot': {
+            msg.reply('artbot v1.2 for commands type \'artbot help\'');
             break;
         }
         case 'artbot help': {
             var help_text = '__**Available commands:**__\n';
-            help_text += '**ping**: test if the bot is working\n';
-            help_text += '**embed test**\n';
-            help_text += '**get channel**: get information about the channel\n';
-            help_text += '**admin test**\n';
-            help_text += '**kick test**\n';
+            help_text += '**artbot ping**: test if the bot is working\n';
+            help_text += '**artbot embed test**\n';
+            help_text += '**artbot get channel**: get information about the channel\n';
+            help_text += '**artbot admin test**\n';
+            help_text += '**artbot kick test**\n';
+            help_text += '**artbot clear assignment submissions**: [admin] clear the assignment submission role manually '
+            help_text += '(ONLY if you know what you\'re doing)\n';
             msg.reply(help_text);
             break;
         }
-        case 'embed test': {
+        case 'artbot ping': {
+            msg.reply('pong');
+            break;
+        }
+        case 'artbot embed test': {
             const embed = new Discord.MessageEmbed()
             .setTitle('A slick little embed')
             .setColor(0xff0000)
@@ -60,19 +69,19 @@ client.on('message', msg => {
             msg.channel.send(embed);
             break;
         }
-        case 'get channel': {
+        case 'artbot get channel': {
             msg.reply('this channel is ' + msg.channel.name + ' id: ' + msg.channel.id);
             break;
         }
-        case 'admin test': {
+        case 'artbot admin test': {
             if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
                 msg.reply('you are an admin');
             } else {
-                msg.reply('you do not have permissions to do that');
+                msg.reply('you do not have permissions to do that (admin)');
             }
             break;
         }
-        case 'kick test': {
+        case 'artbot kick test': {
             if (msg.member.hasPermission(Discord.Permissions.FLAGS.KICK_MEMBERS)) {
                 msg.reply('you can kick people');
             } else {
@@ -80,13 +89,33 @@ client.on('message', msg => {
             }
             break;
         }
+        case 'artbot clear assignment submissions': {
+            if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
+                msg.reply('test');
+            } else {
+                msg.reply('you do not have permissions to do that (admin)');
+            }
+            break;
+        }
+        default: {
+            msg.reply('sorry, your command was not recognized');
+            break;
+        }
     }
 
     // if user posts in submission channel
-    if (submissionList.includes(msg.channel.id) && msg.attachments.size > 0) {
-        msg.reply('your assignment submission has been acknowledged! good job\n\
-        (if you don\'t have the "posted in assignments" role message a mod to report a bot issue)');
-        msg.member.roles.add(submittedRole);
+    if (assignmentList.includes(msg.channel.id) && msg.attachments.size > 0) {
+        // submitted assignment
+        var message = 'your assignment submission has been acknowledged! good job\n';
+        message += '(if you don\'t have the "posted in assignments" role message a mod to report a bot issue)';
+        msg.reply(message);
+        msg.member.roles.add(assignmentSubRole);
+    } else if (msg.channel.id == figuresChannel && msg.attachments.size > 0) {
+        // submitted figures
+        var message = 'your assignment submission has been acknowledged! good job\n';
+        message += '(if you don\'t have the "posted in assignments" role message a mod to report a bot issue)';
+        msg.reply(message);
+        msg.member.roles.add(figuresSubRole);
     }
 });
 
