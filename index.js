ea4456c2c352ab615236e73eaffa71252f8fc4d1
Jonathan <jchiangconan@gmail.com>
2022-07-01 14:41:29 -0400

    new server version

--- index.js
+++ index.js
@@ -7,21 +7,9 @@ var didDailyCheck = false;
 
 // list of assignment submission channels
 const assignmentList = [
-    "859266797680984095", // test channel
-    "853820913955110942", // colour theory
-    "853822141992796191", // environments
-    "853822656531791872", // perspective
-    "853823062968762378", // interiors
-    "853825764419960872", // concept art
-    "853826924455264286", // anatomy
-    "853828342079815740", // character design
-    "859260394900029471", // foreshortening
-    "853829336910397450", // interaction
-    "853829700051664897"  // storyboard
+    "992479016009343046", // test channel
+    "989252740662308934", // assignment share channel
 ];
-
-// figures submission channel
-const figuresChannel = "853855324259745792";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -33,7 +21,7 @@ client.on('message', msg => {
     if (msg.content.startsWith('artbot')){// commands
         switch (msg.content) {
             case 'artbot': {
-                msg.reply('artbot v1.2 for commands type \'artbot help\'');
+                msg.reply('artbot v1.3 for commands type \'artbot help\'');
                 break;
             }
             case 'artbot help': {
@@ -43,9 +31,7 @@ client.on('message', msg => {
                 help_text += '**artbot get channel**: get information about the channel\n';
                 help_text += '**artbot admin test**\n';
                 help_text += '**artbot kick test**\n';
-                help_text += '**artbot clear assignment submissions**: [admin] clear the assignment submission role manually '
-                help_text += '(ONLY if you know what you\'re doing)\n';
-                help_text += '**artbot clear figure submissions**: [admin] clear the figures submission role manually '
+                help_text += '**artbot clear submissions**: [admin] clear the assignment submission role manually '
                 help_text += '(ONLY if you know what you\'re doing)\n';
                 msg.reply(help_text);
                 break;
@@ -84,17 +70,8 @@ client.on('message', msg => {
             }
             case 'artbot clear assignment submissions': {
                 if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-                    clear_role(msg, "posted in assignments");
+                    clear_role(msg, "posted");
                     msg.reply('cleared assignment submissions');
-                } else {
-                    msg.reply('you do not have permissions to do that (admin)');
-                }
-                break;
-            }
-            case 'artbot clear figure submissions': {
-                if (msg.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-                    clear_role(msg, "posted in figures");
-                    msg.reply('cleared figure submissions');
                 } else {
                     msg.reply('you do not have permissions to do that (admin)');
                 }
@@ -111,16 +88,9 @@ client.on('message', msg => {
     if (assignmentList.includes(msg.channel.id) && msg.attachments.size > 0) {
         // submitted assignment
         var message = 'your assignment submission has been acknowledged! good job\n';
-        message += '(if you don\'t have the "posted in assignments" role message a mod to report a bot issue)';
+        message += '(if you don\'t have the "posted" role message an admin to report a bot issue)';
         msg.reply(message);
-        var role = msg.guild.roles.cache.find(role => role.name === "posted in assignments");
-        msg.member.roles.add(role);
-    } else if (msg.channel.id == figuresChannel && msg.attachments.size > 0) {
-        // submitted figures
-        var message = 'your figures submission has been acknowledged! good job\n';
-        message += '(if you don\'t have the "posted in figures" role message a mod to report a bot issue)';
-        msg.reply(message);
-        var role = msg.guild.roles.cache.find(role => role.name === "posted in figures");
+        var role = msg.guild.roles.cache.find(role => role.name === "posted");
         msg.member.roles.add(role);
     }
 });
